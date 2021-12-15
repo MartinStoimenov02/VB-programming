e3ee920b91f5313c6bd6c3bb2bc5b86a5d5a068c
MartinStoimenov02 <mgs_martin_georgiev_02@abv.bg>
2021-12-15 19:46:30 +0200

    new version

---
+++ функционални програми/Тапети/Тапети 2.0/тапети - official 1/Form1.Designer.vb
@@ -0,0 +1,331 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.TextBox12 = New System.Windows.Forms.TextBox()
+        Me.TextBox11 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(90, 340)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(21, 0, 21, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(266, 33)
+        Me.Label5.TabIndex = 80
+        Me.Label5.Text = "обща квадратура"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(381, 333)
+        Me.TextBox4.Margin = New System.Windows.Forms.Padding(21, 18, 21, 18)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(289, 40)
+        Me.TextBox4.TabIndex = 79
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(843, 157)
+        Me.Label15.Margin = New System.Windows.Forms.Padding(21, 0, 21, 0)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(174, 33)
+        Me.Label15.TabIndex = 78
+        Me.Label15.Text = "брой врати"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(129, 168)
+        Me.Label14.Margin = New System.Windows.Forms.Padding(21, 0, 21, 0)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(229, 33)
+        Me.Label14.TabIndex = 77
+        Me.Label14.Text = "брой прозорци"
+        '
+        'TextBox12
+        '
+        Me.TextBox12.Location = New System.Drawing.Point(1059, 150)
+        Me.TextBox12.Margin = New System.Windows.Forms.Padding(21, 18, 21, 18)
+        Me.TextBox12.Name = "TextBox12"
+        Me.TextBox12.Size = New System.Drawing.Size(289, 40)
+        Me.TextBox12.TabIndex = 76
+        '
+        'TextBox11
+        '
+        Me.TextBox11.Location = New System.Drawing.Point(381, 160)
+        Me.TextBox11.Margin = New System.Windows.Forms.Padding(21, 18, 21, 18)
+        Me.TextBox11.Name = "TextBox11"
+        Me.TextBox11.Size = New System.Drawing.Size(289, 40)
+        Me.TextBox11.TabIndex = 75
+        '
+        'Button3
+        '
+        Me.Button3.ForeColor = System.Drawing.Color.Red
+        Me.Button3.Location = New System.Drawing.Point(1059, 246)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(21, 18, 21, 18)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(297, 127)
+        Me.Button3.TabIndex = 74
+        Me.Button3.Text = "изчисти"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.ForeColor = System.Drawing.Color.Red
+        Me.Button2.Location = New System.Drawing.Point(720, 417)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(21, 18, 21, 18)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(636, 128)
+        Me.Button2.TabIndex = 73
+        Me.Button2.Text = "край"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.ForeColor = System.Drawing.Color.Red
+        Me.Button1.Location = New System.Drawing.Point(720, 246)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(21, 18, 21, 18)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(297, 127)
+        Me.Button1.TabIndex = 72
+        Me.Button1.Text = "изчисли"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(192, 513)
+        Me.Label13.Margin = New System.Windows.Forms.Padding(21, 0, 21, 0)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(170, 33)
+        Me.Label13.TabIndex = 71
+        Me.Label13.Text = "обща цена"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(72, 426)
+        Me.Label12.Margin = New System.Windows.Forms.Padding(21, 0, 21, 0)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(283, 33)
+        Me.Label12.TabIndex = 70
+        Me.Label12.Text = "брой ролки тапети"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(57, 254)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(21, 0, 21, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(298, 33)
+        Me.Label9.TabIndex = 69
+        Me.Label9.Text = "цена за една ролка"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(1059, 23)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(21, 0, 21, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(148, 33)
+        Me.Label4.TabIndex = 68
+        Me.Label4.Text = "височина"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(720, 23)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(21, 0, 21, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(124, 33)
+        Me.Label3.TabIndex = 67
+        Me.Label3.Text = "ширина"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(372, 23)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(21, 0, 21, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(143, 33)
+        Me.Label2.TabIndex = 66
+        Me.Label2.Text = "дължина"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(84, 81)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(21, 0, 21, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(268, 33)
+        Me.Label1.TabIndex = 65
+        Me.Label1.Text = "размер на стаята"
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(381, 505)
+        Me.TextBox10.Margin = New System.Windows.Forms.Padding(21, 18, 21, 18)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(289, 40)
+        Me.TextBox10.TabIndex = 64
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(381, 419)
+        Me.TextBox9.Margin = New System.Windows.Forms.Padding(21, 18, 21, 18)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(289, 40)
+        Me.TextBox9.TabIndex = 63
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(381, 246)
+        Me.TextBox8.Margin = New System.Windows.Forms.Padding(21, 18, 21, 18)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(289, 40)
+        Me.TextBox8.TabIndex = 62
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(1059, 74)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(21, 18, 21, 18)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(289, 40)
+        Me.TextBox3.TabIndex = 61
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(720, 74)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(21, 18, 21, 18)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(289, 40)
+        Me.TextBox2.TabIndex = 60
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(381, 74)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(21, 18, 21, 18)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(289, 40)
+        Me.TextBox1.TabIndex = 59
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(694, 211)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(688, 373)
+        Me.PictureBox1.TabIndex = 81
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(18.0!, 33.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1380, 584)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.TextBox12)
+        Me.Controls.Add(Me.TextBox11)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox10)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.ForeColor = System.Drawing.Color.Blue
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(9, 8, 9, 8)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Тапети"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label15 As System.Windows.Forms.Label
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents TextBox12 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox11 As System.Windows.Forms.TextBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox10 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox9 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox8 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ функционални програми/Тапети/Тапети 2.0/тапети - official 1/Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim a, b, h, a1, h1, a2, h2, S, c, c1, br, d, pr, vr As Double
+        Dim shir(20), vis(20) As Double
+        Dim n, m, i As Integer
+        a = TextBox1.Text
+        b = TextBox2.Text
+        h = TextBox3.Text
+        c = TextBox8.Text
+        n = TextBox11.Text
+        m = TextBox12.Text
+        For i = 1 To n Step 1
+            shir(i) = InputBox(a1, "въведи ширина на прозорец")
+            vis(i) = InputBox(h1, "въведи височина на прозорец")
+        Next
+        For i = 1 To n Step 1
+            d = shir(i) * vis(i)
+            pr = d + pr
+        Next
+        d = 0
+        For i = 1 To m Step 1
+            shir(i) = InputBox(a2, "въведи ширина на вратата")
+            vis(i) = InputBox(h2, "въведи височина на вратата")
+        Next
+        For i = 1 To m Step 1
+            d = shir(i) * vis(i)
+            vr = d + vr
+        Next
+        S = ((2 * a * h) + (2 * b * h)) - (pr + vr)
+        TextBox4.Text = S
+        br = S / 5
+        br = Math.Ceiling(br)
+        TextBox9.Text = br
+        c1 = br * c
+        c1 = Math.Round(c1, 2)
+        TextBox10.Text = c1
+    End Sub
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        TextBox1.Text = " "
+        TextBox2.Text = " "
+        TextBox3.Text = " "
+        TextBox4.Text = " "
+        TextBox8.Text = " "
+        TextBox9.Text = " "
+        TextBox10.Text = " "
+        TextBox11.Text = " "
+        TextBox12.Text = " "
+    End Sub
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        End
+    End Sub
+End Class
